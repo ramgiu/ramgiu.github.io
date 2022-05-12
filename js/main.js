b76c56fc33b058c1041f32adabc482d304e8165d
ramgiu <ramiro.giunta@gmail.com>
2022-05-11 23:42:31 -0300

    adding age function

--- js/main.js
+++ js/main.js
@@ -333,3 +333,14 @@
     })();
 
 })(jQuery);
+
+/* age function
+    * ------------------------------------------------------ */
+
+        var birth = new Date('1990-05-12');
+        var now = new Date();
+        var difference = now - birth;
+        var age = new Date(difference).getFullYear() - 1970
+
+        var span = document.getElementById('age');
+        span.innerHTML = age;
